2e651f6c30e544e4c22a06260409d3e3dcc9d3f2
bilalahmadgujjar <153160047+bilalahmadgujjar@users.noreply.github.com>
2025-11-14 00:00:36 +0500

    set app bg color

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF543233), // reddish tint top
+              Color(0xFF0D1021), // deep navy mid
+              Color(0xFF150035), // purple base
+            ],
+            stops: [-2, 0.35, 1.0],
+          ),
+        ),
+      ),
     );
   }
 }
