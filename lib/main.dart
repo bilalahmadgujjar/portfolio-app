cdfa423fb4a4e7c6d338ba05f27b3979c18b3c8e
bilalahmadgujjar <153160047+bilalahmadgujjar@users.noreply.github.com>
2025-11-14 00:15:41 +0500

    navigation color fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:portfolio/screens/homepage/homepage.dart';
 import 'package:portfolio/widget/theme.dart';
+import 'package:portfolio/widget/theme_color.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  /// Set proper edge-to-edge display mode
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   runApp(const MyApp());
 }
 
@@ -11,24 +16,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Portfolio',
-      debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.system, // auto switch based on device
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      home: const Homepage(),
-      builder: (context, child) {
-        // Ensure responsiveness applies to all screens
-        final mediaQuery = MediaQuery.of(context);
-        return MediaQuery(
-          data: mediaQuery.copyWith(
-            textScaler:
-                const TextScaler.linear(1.0), // prevent font scaling issues
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          statusBarColor: AppTheme.brownColor,
+          statusBarIconBrightness: Brightness.light,
+          statusBarBrightness: Brightness.dark,
+
+          ///=========================================================================
+          ///========================== Navigation bar ===========================
+          ///========================================================================
+
+          systemNavigationBarColor:
+              AppTheme.deepColor, //navigation bar background
+          systemNavigationBarIconBrightness:
+              Brightness.dark, // navigation bar icons
+        ),
+        child: Container(
+          color: AppTheme.navyColor,
+          child: SafeArea(
+            child: MaterialApp(
+              title: 'Portfolio',
+              debugShowCheckedModeBanner: false,
+              themeMode: ThemeMode.system, // auto switch based on device
+              theme: lightTheme,
+              darkTheme: darkTheme,
+              home: const Homepage(),
+              builder: (context, child) {
+                // Ensure responsiveness applies to all screens
+                final mediaQuery = MediaQuery.of(context);
+                return MediaQuery(
+                  data: mediaQuery.copyWith(
+                    textScaler: const TextScaler.linear(
+                        1.0), // prevent font scaling issues
+                  ),
+                  child: child!,
+                );
+              },
+            ),
           ),
-          child: child!,
-        );
-      },
-    );
+        ));
   }
 }
